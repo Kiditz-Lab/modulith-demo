c9d173d66c6aad8382b894c6a5cb1bdd724d37cf
rifkyaditya <kiditzbastara@gmail.com>
2025-03-09 22:49:12 +0700

    remove flyway

--- src/main/java/com/example/demo/orders/Orders.java
+++ src/main/java/com/example/demo/orders/Orders.java
@@ -1,17 +1,15 @@
 package com.example.demo.orders;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 class Orders {
 	private final OrderRepository repository;
-
-	Orders(OrderRepository repository) {
-		this.repository = repository;
-	}
 
 	void placed(OrderPlacedCommand command) {
 		Set<LineItem> itemSet = command.items().stream().map(item -> new LineItem(null, item.sku(), item.quantity())).collect(Collectors.toSet());
